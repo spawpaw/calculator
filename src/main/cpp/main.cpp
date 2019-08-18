555a83d6c0d54f8c635c2a8b06a3a681534acc99
spawpaw <spawpaw@hotmail.com>
2019-08-18 14:45:40 +0800

    fix: 无法识别程序参数的问题

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -12,7 +12,9 @@ int main(int argc, char *argv[]) {
     CalculatorLA calculatorLa = CalculatorLA();
     string s;
     if (argc > 1) {
-        s = argv[1];
+        for (int i = 1; i < argc; ++i) {
+            s += argv[i];
+        }
     } else {
         cout << "please input your expression(e.g: 1+2*(1+9)/5):";
         cin >> s;

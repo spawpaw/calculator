73f7e9ac1d4a7334d97fc56f942d288711b933ae
spawpaw <spawpaw@hotmail.com>
2019-08-18 14:34:23 +0800

    feat: 构建主程序

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -2,29 +2,23 @@
 // Created by C9D0B1BC on 2019/8/16.
 //
 
-#include <cstdio>
 #include <calculator.h>
 #include "string"
+#include "iostream"
 
 using namespace std;
 
-//int main(int argc, char *argv[]) {
-//    CalculatorLA calculatorLa = CalculatorLA();
-//    if (argc > 1) {
-//        calculatorLa.nextString(string(argv[0]));
-//    } else {
-//        char *buf = new char[1024];
-//        scanf("please input your expression:\n%s", buf);
-//        calculatorLa.nextString(string(buf));
-//    }
-//
-//    double result = calculatorLa.calculate();
-//    printf("result: %lf", result);
-//
-//    return 0;
-//}
-
-int main() {
+int main(int argc, char *argv[]) {
     CalculatorLA calculatorLa = CalculatorLA();
+    string s;
+    if (argc > 1) {
+        s = argv[1];
+    } else {
+        cout << "please input your expression(e.g: 1+2*(1+9)/5):";
+        cin >> s;
+    }
+    calculatorLa.nextString(s);
+    double result = calculatorLa.calculate();
+    cout << result << endl;
     return 0;
 }
